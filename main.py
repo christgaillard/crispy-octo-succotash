0a13c6945b30ecf29026445185973b6506b36548
Christophe GAILLARD <gaillard@brocelia.fr>
2018-10-29 09:36:15 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import urllib3
+from urllib3 import PoolManager
+http = urllib3.PoolManager()  # type: PoolManager
+import cv2
+import numpy as np
+import os
+
+
+def store_raw_images():
+    neg_images_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'
+    neg_image_urls = http.request('GET', neg_images_link).data.decode('utf-8')
+    pic_num = 1
+
+    if not os.path.exists('neg'):
+        os.makedirs('neg')
+
+    for i in neg_image_urls.split('\n'):
+        try:
+            print(i)
+            http.request('GET',i)
+            open("neg/" + str(pic_num) + ".jpg",'wb')
+            img = cv2.imread("neg/" + str(pic_num) + ".jpg", cv2.IMREAD_GRAYSCALE)
+            # should be larger than samples / pos pic (so we can place our image on it)
+            resized_image = cv2.resize(img, (100, 100))
+            cv2.imwrite("neg/" + str(pic_num) + ".jpg", resized_image)
+            pic_num += 1
+
+        except Exception as e:
+            print(str(e))
+
+
+store_raw_images()
